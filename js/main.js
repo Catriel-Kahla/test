bf0377f45bf5e6bcc047d8a142c39dce2c5dbef4
Catriel Gabriel Kahla <94947330+Catriel-Kahla@users.noreply.github.com>
2023-12-18 15:15:49 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ console.log("001");
 
 
 document.getElementById("goHome").addEventListener('click', () => {
-    location.href = "../index.html";
+    location.href = "index.html";
 });
 
 document.getElementById("n001").addEventListener('click', () => {
-    location.href = "../pages/n_001.html";
+    location.href = "pages/n_001.html";
 });
 
 
